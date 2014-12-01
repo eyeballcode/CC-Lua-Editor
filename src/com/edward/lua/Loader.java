c6326bb30000a472705346c6047425e6d05f2ce0
Eyeball Edward <edwardyeung.private@gmail.com>
2014-12-01 18:02:50 +0800

    Cleaned up Loader class

--- src/com/edward/lua/Loader.java
+++ src/com/edward/lua/Loader.java
@@ -23,50 +23,78 @@ public class Loader extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	JButton done = null;
+	JLabel label = null;
 
-	public Loader() {
-		JButton done = new JButton("Done") {@Override public void setBorder(Border b) {}};
+	private JButton done() {
+		done = new JButton("Done") {
+			private static final long serialVersionUID = -3089163832904127158L;
+
+			@Override
+			public void setBorder(Border b) {
+			}
+		};
 		done.setBackground(new Color(204, 76, 76));
-		done.repaint();
 		done.setOpaque(true);
 		done.setForeground(Color.WHITE);
 		done.setSize(new Dimension(100, 30));
-		done.setLocation(400,  80);
+		done.setLocation(400, 80);
 		done.setFont(new Font("Minecraftia", Font.PLAIN, 17));
+		return done;
+	}
+
+	private JTextField tf() {
+
 		tf = new JTextField() {
 			private static final long serialVersionUID = 1L;
-			@Override public void setBorder(Border border) {}
+
+			@Override
+			public void setBorder(Border border) {
+			}
 		};
-		
+
 		tf.setBackground(Color.BLACK);
 		tf.setForeground(Color.WHITE);
 		tf.setVisible(true);
 		tf.setSize(new Dimension(305, 50));
 		tf.setLocation(340, 21);
 		tf.setFont(new Font("Minecraftia", Font.PLAIN, 17));
-		done.addActionListener(new ActionListener() {
+		return tf;
+	}
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				if(!tf.getText().equals("")) {
-				System.out.println("Project name is: "+tf.getText());
-				} else {
-					System.out.println("Project name cannot be empty");
-				}
-			}
-			
-		});
-		JLabel label = new JLabel("Please enter project name: ");
+	private JLabel label() {
+		label = new JLabel("Please enter project name: ");
 		label.setVisible(true);
 		label.setForeground(Color.WHITE);
 		label.setSize(new Dimension(500, 50));
 		label.setLocation(25, 17);
 		label.setFont(new Font("Minecraftia", Font.PLAIN, 17));
+		return label;
+	}
+	private void setUpGUI() {
+
 		setUndecorated(false);
 		setContentPane(new JLabel(new ImageIcon(getClass().getResource(
 				"ComBorder.png"))));
-		add(label);	
-		add(tf);
+		add(label());
+		add(tf());
+	}
+
+	public Loader() {
+		JButton done = done();
+		done.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (!tf.getText().equals("")) {
+					System.out.println("Project name is: " + tf.getText());
+				} else {
+					System.out.println("Project name cannot be empty");
+				}
+			}
+
+		});
+		setUpGUI();
 		add(done);
 		pack();
 		setVisible(true);
